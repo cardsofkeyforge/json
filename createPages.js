af177e8ecae125cf26c50a326d9660b135976fdf
Alfredo Fialho <alfoltran@gmail.com>
2020-03-11 12:15:04 -0300

    :recycle: Improving page generation via template engine.

--- createPages.js
+++ createPages.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const ejs = require("ejs");
 
 const configs = require('./config');
 const messages = require('./messages');
@@ -9,22 +10,18 @@ const rarities = messages[configs.lang].rarities;
 
 function createPage(card, expansion) {
     const cardPage = './pages/' + expansion.lang + '/' + expansion.name + '/' + card.card_number + '.md';
-    fs.writeFileSync(cardPage, '---\ntitle: ' + expansion.longname + '\nsubtitle: ' + expansion.releaseDate + '\nlayout: page\nshow_sidebar: false\nhero_image: ' + expansion.image + '\n---\n\n', {flag: 'w'});
-    fs.writeFileSync(cardPage, '![' + card.card_number + '](' + card.front_image + ')\n\n', {flag: 'a'});
-    fs.writeFileSync(cardPage, '## ' + card.card_title + '\n\n', {flag: 'a'});
-    fs.writeFileSync(cardPage, '|----|----|\n', {flag: 'a'});
-    fs.writeFileSync(cardPage, '| ID | ' + card.id + ' |\n', {flag: 'a'});
-    fs.writeFileSync(cardPage, '| Número | ' + card.card_number + ' |\n', {flag: 'a'});
-    if (!card.is_anomaly) fs.writeFileSync(cardPage, '| Casa | ![' + card.house + '](' + houses[card.house].image + ' "' + houses[card.house].name + '") |\n', {flag: 'a'});
-    fs.writeFileSync(cardPage, '| Tipo | ' + types[card.card_type] + ' |\n', {flag: 'a'});
-    if (card.traits) fs.writeFileSync(cardPage, '| Atributos | ' + card.traits + ' |\n', {flag: 'a'});
-    if (card.amber) fs.writeFileSync(cardPage, '| Bônus | ' + card.amber + 'A |\n', {flag: 'a'});
-    if (card.card_type === 'Creature') fs.writeFileSync(cardPage, '| Poder | ' + card.power + ' |\n', {flag: 'a'});
-    if (card.card_type === 'Creature') fs.writeFileSync(cardPage, '| Armadura | ' + (card.armor ? card.armor : '0') + ' |\n', {flag: 'a'});
-    if (card.card_text) fs.writeFileSync(cardPage, '| Texto | ' + card.card_text.replace(/_x000D_/, ' ').replace(/\n/, ' ').replace(/\r/, ' ') + ' |\n', {flag: 'a'});
-    fs.writeFileSync(cardPage, '| Raridade | ' + rarities[card.rarity] + ' |\n', {flag: 'a'});
-    if (card.flavor_text) fs.writeFileSync(cardPage, '| Dizeres | ' + card.flavor_text.replace(/_x000D_/, ' ').replace(/\n/, ' ').replace(/\r/, ' ') + ' |', {flag: 'a'});
-    if (card.errata) fs.writeFileSync(cardPage, '\n### Errata\n\n' + card.errata, {flag: 'a'})
+
+    ejs.renderFile('./pages/' + expansion.lang + '/page.ejs', {
+        expansion: expansion,
+        card: card,
+        house: card.is_anomaly ? null : houses[card.house],
+        type: types[card.card_type],
+        text: card.card_text ? card.card_text.replace(/_x000D_/g, ' ').replace(/\n/g, ' ').replace(/\r/g, ' ') : null,
+        rarity: rarities[card.rarity],
+        flavor: card.flavor_text ? card.flavor_text.replace(/_x000D_/g, ' ').replace(/\n/g, ' ').replace(/\r/g, ' ') : null
+    }, {filename: cardPage}, function(err, str) {
+        if (!err) fs.writeFileSync(cardPage, str, {flag: 'w'});
+    });
 }
 
 configs.expansion.forEach(expansion => {
