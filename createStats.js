ae97e7a6657a65b141abe8444cc079a652bdbfe8
Alfredo Fialho <alfoltran@gmail.com>
2020-08-09 16:50:40 -0300

    :zap: Adding same stats.

--- createStats.js
+++ createStats.js
@@ -2,16 +2,27 @@ const fs = require("fs");
 
 const configs = require("./config");
 
+const keyforge = {};
+const set = {};
 configs.expansion.forEach(expansion => {
   console.log("Loading cards from " + expansion.longname + "...");
   const cardsFolder = "./json/" + expansion.lang + "/" + expansion.name + "/";
   const cards = fs.readdirSync(cardsFolder);
   console.log("Setup to processing " + cards.length + " cards...");
-  const set = {};
+  set[expansion.name] = {};
   cards.forEach(file => {
     let card = JSON.parse(fs.readFileSync(cardsFolder + file).toString());
-    if (!set[card.card_title]) set[card.card_title] = [];
-    set[card.card_title].push(card);
+    if (!keyforge[card.card_title]) keyforge[card.card_title] = [];
+    if (!set[expansion.name][card.card_title]) {
+      set[expansion.name][card.card_title] = [];
+    }
+
+    keyforge[card.card_title].push(card);
+    set[expansion.name][card.card_title].push(card);
   });
-  console.log(Object.keys(set).length + " different cards!");
+  console.log(Object.keys(set[expansion.name]).length + " different cards!");
 });
+
+console.log(
+  `KeyForge has ${Object.keys(keyforge).length} total different cards!`
+);
