f7f20025486a4891c60c5ead4290088dd9b9d8ec
Alfredo Fialho <alfoltran@gmail.com>
2021-05-11 21:45:37 -0300

    :art: Enhancement adjusts.

--- decks/tts/Global.lua
+++ decks/tts/Global.lua
@@ -92,7 +92,7 @@ function addAembers(card)
             local enhancement = {
                 name     = "Amber",
                 url      = "http://cloud-3.steamusercontent.com/ugc/1327949551204024646/6447043575CF9CE0B6A8BBA125CEB37D0441A103/",
-                position = {0.8, 0.45, -0.92 + 0.22 * (idx - 1)},
+                position = {0.81, 0.45, -0.92 + 0.22 * (idx - 1)},
                 rotation = {90, 180, 0},
                 scale    = {0.23602064, 0.188157976, 4.342106},
             }
@@ -109,7 +109,7 @@ function addCaptures(card)
             local enhancement = {
                 name     = "Capture",
                 url      = "http://cloud-3.steamusercontent.com/ugc/1327949551204021496/C8DA8AECB454FDD49B9F59600339827FE82C32EE/",
-                position = {0.8, 0.45, -0.92 + 0.22 * (idx - 1)},
+                position = {0.81, 0.45, -0.92 + 0.22 * (idx - 1)},
                 rotation = {90, 180, 0},
                 scale    = {0.2238137, 0.206972957, 4.776311},
             }
@@ -126,7 +126,7 @@ function addDamages(card)
             local enhancement = {
                 name     = "Damage",
                 url      = "http://cloud-3.steamusercontent.com/ugc/1327949551204023515/9CD61ADB57C87691683808B65F85F3D2FFDE6BEF/",
-                position = {0.8, 0.45, -0.92 + 0.22 * (idx - 1)},
+                position = {0.81, 0.45, -0.92 + 0.22 * (idx - 1)},
                 rotation = {90, 180, 0},
                 scale    = {0.223114237, 0.227670163, 5.2539463},
             }
@@ -143,7 +143,7 @@ function addDraws(card)
             local enhancement = {
                 name     = "Draw",
                 url      = "http://cloud-3.steamusercontent.com/ugc/1327949551204011885/4EFBF9050F2709451F9004F6D3778D0DD15FDFA7/",
-                position = {0.8, 0.45, -0.92 + 0.22 * (idx - 1)},
+                position = {0.83, 0.45, -0.92 + 0.22 * (idx - 1)},
                 rotation = {90, 180, 0},
                 scale    = {0.196684808, 0.2069729, 4.776312},
             }
